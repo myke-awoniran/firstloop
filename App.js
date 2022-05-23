e503f9a9f6a66ee5dd5ce89967c083bc372fbe26
Awoniran <awoniranopeyemi@gmail.com>
2022-05-23 06:45:19 +0100

    finish robust error handling

--- App.js
+++ App.js
@@ -3,14 +3,20 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const app = require('express')();
 const bodyParser = require('body-parser');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const mongoSanitize = require('express-mongo-sanitize');
 const version1 = require('./src/versions/version1');
 const App = require('./src/controllers/App/appController');
 const errHandler = require('./src/controllers/err/Global Error/Global_error_handler');
 
 app.use(cors());
 app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
 app.use(morgan('combined'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10kb' }));
+app.use(hpp());
 
 app.get('/', App.HttpHomeController);
 
